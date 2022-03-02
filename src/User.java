2766533593d2a38c86e948c7f78b787c2dc96792
Key-Locker <mathewmigliore@hotmail.com>
2022-03-02 16:16:33 -0500

    Update User.java

--- src/User.java
+++ src/User.java
@@ -71,7 +71,7 @@ public class User {
         else if (username.length() > 15 || username.length() < 1){
             return false; 
         }
-        else if (!username.matches("^[-a-zA-Z0-9_]*$+")){
+        else if (!username.matches("^[-a-zA-Z0-9_ ]*$")){
             return false; 
         }
         else{
