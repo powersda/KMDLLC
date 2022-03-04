8472e86cd04dc57f62600436e39b57f8c538a69c
David Powers <david.powers1@ontariotechu.net>
2022-03-04 18:04:18 -0500

    changed java.util.Data to java.util.Date...

--- src/DataAccess.java
+++ src/DataAccess.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Data;
+import java.util.Date;
 import java.text.DateFormat;
 
 public class DataAccess {
