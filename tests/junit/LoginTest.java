0c315d48aca69dfe706580fb6da33dfba2de259c
David Powers <david.powers1@ontariotechu.net>
2022-03-23 17:43:41 -0400

    Added junit jar and started building LoginTest class

---
+++ tests/junit/LoginTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class LoginTest extends Login {
+
+    @Test 
+    public void alwaysTrueTest() {
+        assertTrue(true);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
